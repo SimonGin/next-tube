137786ce54b33ca5053269b012bcb8c2ad4d514b
SimonGin <tt26419@gmail.com>
2026-01-19 10:26:20 +0700

    category module: schema, prefetch and ui carousel component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,22 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { PageClient } from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
-const HomePage = async () => {
-  void trpc.hello.prefetch({ text: "Simon" });
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{ categoryId?: string }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ErrorBoundary fallback={<p>Error...</p>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
 };
 
-export default HomePage;
+export default Page;
