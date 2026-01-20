d76ff428e4487f2b5a7848e4302beded2624d3ae
SimonGin <tt26419@gmail.com>
2026-01-20 09:52:40 +0700

    video and studio modules: schemas, procedures, video records

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,11 @@
-import { z } from "zod";
-import { protectedProcedure, createTRPCRouter } from "../init";
-import { categories } from "@/db/schema";
+import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { studioRouter } from "@/modules/studio/server/procedures";
+import { videosRouter } from "@/modules/videos/server/procedures";
+
 export const appRouter = createTRPCRouter({
+  studio: studioRouter,
+  videos: videosRouter,
   categories: categoriesRouter,
 });
 // export type definition of API
